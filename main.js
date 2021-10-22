6837d257afbb945a0a604869e9d300cb4d184b82
Diogo Delazare <88203023+JoemanJ@users.noreply.github.com>
2021-10-22 01:20:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const links = {
+               Instagram: "JoemanJ34",
+               Discordapp: "users/248514151071088641",
+               Github: "JoemanJ"
+               }
+
+function getGithubInfo(){
+    const url = `https://api.github.com/users/${links.Github}`
+
+    fetch(url).then(response => response.json()).then(json => {
+        myName.textContent = json.name
+        profilePicture.src = json.avatar_url
+        bio.textContent = json.bio
+        githubUsername.textContent = json.login
+    })
+}
+
+function changeLinks(){
+    const lista = document.getElementById("links")
+
+    for(let li of lista.children){
+        const app = li.children[0].getAttribute('id')
+
+        li.children[0].href = `https://${app}.com/${links[app]}`
+
+        //alert(li.children[0].href)
+    }
+}
+
+changeLinks()
+getGithubInfo()
